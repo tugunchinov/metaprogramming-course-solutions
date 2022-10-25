c401d72a6932c2f3911ef6ca925d1cb9de75201b
Maxim Tugunchinov <tugunchinov.maxim@gmail.com>
2022-10-25 11:54:03 +0000

    task 22 draft

--- task2/PolymorphicMapper.hpp
+++ task2/PolymorphicMapper.hpp
@@ -2,11 +2,36 @@
 
 #include <optional>
 
-
 template <class From, auto target>
-struct Mapping;
+struct Mapping {
+  using Type = From;
+  static constexpr auto Target = target;
+};
 
 template <class Base, class Target, class... Mappings>
-struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& object);
+class PolymorphicMapper {
+  template <class C, class Map, class... Maps>
+  static std::optional<Target> getTarget(const Base& object)
+    requires std::derived_from<typename Map::Type, Base>
+  {
+    if (dynamic_cast<const typename Map::Type*>(&object) != nullptr) {
+      return Map::Target;
+    } else {
+      return getTarget<C, Maps...>(object);
+    }
+  }
+
+  template<class C>
+  static std::optional<Target> getTarget(const Base& object) {
+    if (dynamic_cast<const typename C::Type*>(&object) != nullptr) {
+      return C::Target;
+    } else {
+      return std::nullopt;
+    }
+  }
+
+ public:
+  static std::optional<Target> map(const Base& object) {
+    return getTarget<Mapping<Base, std::nullopt>, Mappings...>(object);
+  }
 };
